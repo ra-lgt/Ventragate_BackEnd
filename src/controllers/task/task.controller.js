1e99473d48ec207a173e9416fdd7a1302de41422
ra-lgt <raviajay9344@gmail.com>
2025-11-12 19:39:30 +0530

    updated the routes

--- src/controllers/task/task.controller.js
+++ src/controllers/task/task.controller.js
@@ -15,11 +15,20 @@ export const getTasks = async (req, res, next) => {
 export const addTask = async (req, res, next) => {
   try {
     const { title, description, endDate } = req.body;
-    if (!title) return res.status(400).json({ success: false, message: "Title is required" });
+    if (!title)
+      return res
+        .status(400)
+        .json({ success: false, message: "Title is required" });
 
     const newTask = new Task({ title, description, endDate });
     const savedTask = await newTask.save();
-    res.status(201).json({ success: true, message: "Task added successfully" });
+    res
+      .status(201)
+      .json({
+        success: true,
+        message: "Task added successfully",
+        data: savedTask,
+      });
   } catch (error) {
     next(error);
   }
@@ -28,9 +37,16 @@ export const addTask = async (req, res, next) => {
 export const updateTask = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const updatedTask = await Task.findByIdAndUpdate(id, req.body, { new: true });
-    if (!updatedTask) return res.status(404).json({ success: false, message: "Task not found" });
-    res.status(200).json({ success: true, message: "Task updated successfully"});
+    const updatedTask = await Task.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!updatedTask)
+      return res
+        .status(404)
+        .json({ success: false, message: "Task not found" });
+    res
+      .status(200)
+      .json({ success: true, message: "Task updated successfully",data:updatedTask });
   } catch (error) {
     next(error);
   }
@@ -40,28 +56,32 @@ export const deleteTask = async (req, res, next) => {
   try {
     const { id } = req.params;
     const deleted = await Task.findByIdAndDelete(id);
-    if (!deleted) return res.status(404).json({success: false, message: "Task not found" });
-    res.status(200).json({ success: true, message: "Task deleted successfully" });
+    if (!deleted)
+      return res
+        .status(404)
+        .json({ success: false, message: "Task not found" });
+    res
+      .status(200)
+      .json({ success: true, message: "Task deleted successfully" });
   } catch (error) {
     next(error);
   }
 };
-
 
 export const updateStatus = async (req, res, next) => {
   try {
     const { id } = req.params;
     const { status } = req.body;
     if (!["pending", "completed"].includes(status)) {
-      return res.status(400).json({ success: false, message: "Invalid status value" });
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid status value" });
     }
-    const task = await Task.findByIdAndUpdate(
-      id,
-      { status },
-      { new: true }
-    );
+    const task = await Task.findByIdAndUpdate(id, { status }, { new: true });
     if (!task) {
-      return res.status(404).json({success: false, message: "Task not found" });
+      return res
+        .status(404)
+        .json({ success: false, message: "Task not found" });
     }
     res.status(200).json({
       success: true,
@@ -72,4 +92,3 @@ export const updateStatus = async (req, res, next) => {
     next(error);
   }
 };
-
